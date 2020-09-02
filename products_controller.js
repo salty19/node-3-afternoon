549fbca0148e80ada09aad6c24b8dc457bb4a6bb
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-02 15:45:17 -0700

    product_controller methods complete

---
+++ products_controller.js
@@ -0,0 +1,57 @@
+module.exports = {
+
+    create: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.create_product().then( () => 
+        res.sendStatus(200) )
+        .catch( err => {
+            res.status(500).send(
+                {errorMessage: "Something went wrong with the CREATE method."})
+                console.log(err)
+        })
+    },
+
+    getOne: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.read_product().then(product => res.status(200).send(product))
+        .catch(err => {res.status(500).send({
+            errorMessage: "Something went wrong with the GETONE method." })
+            console.log(err)
+        })
+    },
+
+    getAll: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.read_products().then(products => res.status(200).send(products))
+        .catch(err => {
+            res.status(500).send(
+            {errorMessage: "Something went wrong with the GETALL method."})
+            console.log(err)
+        })
+    },
+
+    update: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.update_product().then( () => res.sendStatus(200))
+        .catch(err => {
+            res.status(500).send(
+            {errorMessage: "Something went wrong with the UPDATE method."})
+            console.log(err)
+        })
+    },
+
+    delete: (req, res, next) => {
+        const dbInstance = req.app.get('db')
+
+        dbInstance.delete_product().then( () => res.sendStatus(200))
+        .catch( err => {
+            res.status(500).send({
+                errorMessage: "Something went wrong with DELETE method."})
+                console.log(err)
+        })
+    }
+}
