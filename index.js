a899165daea21cc508335d0667be23c379f6abb1
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-02 14:50:34 -0700

    Step 2 complete

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const massive = require('massive')
 
 const app = express()
 
-const {SERVER_PORT} = process.env
+const {SERVER_PORT, CONNECTION_STRING} = process.env
 
 app.use(express.json())
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set('db', dbInstance)
+}).catch(err => console.log(err))
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}!`)
