9037021d0d510670b60671d908e5ffbe88c8f91c
Kara LeSueur <kara.lesueur@gmail.com>
2020-09-02 18:08:08 -0700

    api's connected on index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const massive = require('massive')
+const products_controller = require('./products_controller')
 
 const app = express()
 
@@ -14,6 +15,12 @@ massive({
 }).then(dbInstance => {
     app.set('db', dbInstance)
 }).catch(err => console.log(err))
+
+app.post('/api/products', products_controller.create)
+app.get('/api/products', products_controller.getAll)
+app.get('/api/products/:id', products_controller.getOne)
+app.put('/api/products/:id', products_controller.update)
+app.delete('api/products/:id', products_controller.delete)
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}!`)
